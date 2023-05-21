ef6915ca17aa0288d38c8ba5d1c1f0c3809f43d9
Emin <eminyilmzz@gmail.com>
2023-05-21 22:58:38 +0300

    part two solution

--- 2015/01/main.go
+++ 2015/01/main.go
@@ -7,11 +7,16 @@ import (
 )
 
 func main() {
+	fmt.Println(partOne())
+	fmt.Println(partTwo())
+}
+
+func partOne() (int, error) {
 	file, err := os.Open("input.txt")
 	pos := 0
 	if err != nil {
 		fmt.Println("Error opening file:", err)
-		return
+		return 0, err
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
@@ -25,5 +30,31 @@ func main() {
 			}
 		}
 	}
+	return pos, nil
+}
+
+func partTwo() int {
+	file, err := os.Open("input.txt")
+	pos := 0
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return 0
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		data := scanner.Text()
+		for i, char := range data {
+			if char == '(' {
+				pos++
+			} else {
+				pos--
+			}
+			if pos == -1 {
+				return i + 1
+			}
+		}
+	}
 	fmt.Println(pos)
+	return -1
 }
