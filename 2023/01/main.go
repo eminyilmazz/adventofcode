ec98c3a8df8080dc6048717b2875136c09fc7850
Emin <eminyilmzz@gmail.com>
2023-12-07 06:34:23 +0300

    2023/01 part 2

--- 2023/01/main.go
+++ 2023/01/main.go
@@ -9,8 +9,21 @@ import (
 	"unicode"
 )
 
+var words = map[string]rune{
+	"one":   '1',
+	"two":   '2',
+	"three": '3',
+	"four":  '4',
+	"five":  '5',
+	"six":   '6',
+	"seven": '7',
+	"eight": '8',
+	"nine":  '9',
+}
+
 func main() {
 	fmt.Println(partOne())
+	fmt.Println(partTwo())
 }
 
 func partOne() int {
@@ -21,6 +34,35 @@ func partOne() int {
 		sum += extractValue(digits)
 	}
 	return sum
+}
+
+func partTwo() int {
+	lines := readInput("input.txt")
+
+	var result int
+	for _, line := range lines {
+		numChars := []rune{}
+		lineLength := len(line)
+		for index, char := range line {
+			if unicode.IsDigit(char) {
+				numChars = append(numChars, char)
+			} else {
+				for key, val := range words {
+					if index+len(key) <= lineLength && line[index:index+len(key)] == key {
+						numChars = append(numChars, val)
+					}
+				}
+			}
+		}
+		first := numChars[0]
+		last := numChars[len(numChars)-1]
+		intValue, err := strconv.Atoi(fmt.Sprintf("%c%c", first, last))
+		if err != nil {
+			log.Fatal(err)
+		}
+		result += intValue
+	}
+	return result
 }
 
 func readInput(dest string) []string {
